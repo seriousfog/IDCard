c0c68606893d2933193324cd69e347712555f881
t9uo <t9uo@fresnou.org>
2023-09-15 09:28:20 -0700

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,46 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.post('/card', function (req, res){
+  console.log(req.body.dateOfBirth)
+  res.render('card',{
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    type: req.body.type,
+    dateOfBirth: new Date(req.body.dateOfBirth),
+    addressLine1:req.body.addressLine1,
+    addressLine2:req.body.addressLine2,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    accountNumber: createAccountNumber(),
+
+    currentDate: new Date(),
+    cardClass: getCardClass(req.body.type)
+  })
+})
 module.exports = router;
+
+function createAccountNumber(){
+  let accNum = ""
+  for (let i = 0; i<5; i++){
+    let temp = Math.floor(Math.random()*10);
+    accNum += temp
+  }
+  return accNum;
+}
+
+function getCardClass(type) {
+  if (type === "Premium") {
+    return "premium"
+  } else if (type === "Standard") {
+    return "standard"
+  } else if (type === "Bronze") {
+    return "bronze"
+  } else {
+    return ""
+  }
+}
